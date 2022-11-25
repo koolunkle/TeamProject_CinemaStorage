18bc8a80ffbdcd55bed4c78ba1ae889eca03b715
koolunkle <a43361904@gmail.com>
2022-11-25 17:12:57 +0900

    [create] ViewPager2, BottomNavigation 연동

---
+++ app/src/main/java/kr/or/mrhi/cinemastorage/view/activity/MainActivity.kt
@@ -0,0 +1,81 @@
+package kr.or.mrhi.cinemastorage.view.activity
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
+import kr.or.mrhi.cinemastorage.R
+import kr.or.mrhi.cinemastorage.databinding.ActivityMainBinding
+import kr.or.mrhi.cinemastorage.view.adapter.PagerAdapter
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+
+    private val binding get() = _binding!!
+
+    private var backPressedTime: Long = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.apply {
+            setViewPager()
+            navigationSelected()
+        }
+    }
+
+    private fun setViewPager() {
+        binding.viewpager.apply {
+            adapter = PagerAdapter(this@MainActivity)
+            registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    binding.bottomNavigation.menu.getItem(position).isChecked = true
+                }
+            })
+        }
+    }
+
+    private fun navigationSelected() {
+        binding.bottomNavigation.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.nav_list -> {
+                    binding.viewpager.currentItem = LIST_FRAGMENT
+                    true
+                }
+                R.id.nav_review -> {
+                    binding.viewpager.currentItem = REVIEW_FRAGMENT
+                    true
+                }
+                else -> {
+                    binding.viewpager.currentItem = PROFILE_FRAGMENT
+                    true
+                }
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (System.currentTimeMillis() - backPressedTime >= 1500) {
+            backPressedTime = System.currentTimeMillis()
+            Toast.makeText(
+                this, resources.getString(R.string.toast_back_pressed), Toast.LENGTH_SHORT
+            ).show()
+        } else finish()
+    }
+
+    override fun onDestroy() {
+        _binding = null
+        super.onDestroy()
+    }
+
+    companion object {
+        const val LIST_FRAGMENT = 0
+        const val REVIEW_FRAGMENT = 1
+        const val PROFILE_FRAGMENT = 2
+    }
+
+}
