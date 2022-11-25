94d6620fc0bcfe185133dd16627f3fdc3ee8030d
koolunkle <a43361904@gmail.com>
2022-11-25 17:11:19 +0900

    [create] PagerAdapter 추가

---
+++ app/src/main/java/kr/or/mrhi/cinemastorage/view/adapter/PagerAdapter.kt
@@ -0,0 +1,19 @@
+package kr.or.mrhi.cinemastorage.view.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import kr.or.mrhi.cinemastorage.view.fragment.ListFragment
+import kr.or.mrhi.cinemastorage.view.fragment.ProfileFragment
+import kr.or.mrhi.cinemastorage.view.fragment.ReviewFragment
+
+class PagerAdapter(activity: FragmentActivity) : FragmentStateAdapter(activity) {
+
+    private val fragmentList = arrayListOf(ListFragment(), ReviewFragment(), ProfileFragment())
+
+    override fun createFragment(position: Int): Fragment {
+        return fragmentList[position]
+    }
+
+    override fun getItemCount() = fragmentList.size
+}
